df40667f7b0dcec9e18e5d01f0e62e16a3029e75
Tomasz Subik <tsubik@gmail.com>
2021-05-14 10:50:28 +0200

    specs refactor

--- backend/spec/services/csv_import/organizations_spec.rb
+++ backend/spec/services/csv_import/organizations_spec.rb
@@ -15,18 +15,4 @@ describe CSVImport::Organizations do
     expect(Region.count).to eq(3)
     expect(BusinessType.count).to eq(4)
   end
-
-  def fixture_file(filename, content: nil, content_type: 'text/csv')
-    file_path = if content.present?
-                  file_path = "#{Rails.root}/tmp/#{filename}"
-                  File.open(file_path, 'w:UTF-8') do |f|
-                    f.write(content)
-                  end
-                  file_path
-                else
-                  "#{Rails.root}/text/fixtures/files/#{filename}"
-                end
-
-    Rack::Test::UploadedFile.new(file_path, content_type)
-  end
 end
