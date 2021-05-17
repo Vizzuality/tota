fc3a4af933658f1f2733e76eb54d25af32b2bc3e
Tomasz Subik <tsubik@gmail.com>
2021-05-17 17:23:16 +0200

    wip importer indicator values

--- backend/db/migrate/20210513182355_create_indicator_values.rb
+++ backend/db/migrate/20210513182355_create_indicator_values.rb
@@ -2,6 +2,7 @@ class CreateIndicatorValues < ActiveRecord::Migration[6.1]
   def change
     create_table :indicator_values do |t|
       t.belongs_to :indicator, null: false, foreign_key: true, index: true
+      t.string :date
       t.string :region
       t.string :category_1
       t.string :category_2
