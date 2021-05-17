0fb712a6092a405f9327a712ddf894b0fb8caf42
Tomasz Subik <tsubik@gmail.com>
2021-05-17 16:04:07 +0200

    region name not null

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_182355) do
   end
 
   create_table "regions", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "parent_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
