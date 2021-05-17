ba306e21faed4e711e264b7a841eb77166afcb71
Tomasz Subik <tsubik@gmail.com>
2021-05-17 16:07:20 +0200

    couple more validations and specs

--- backend/spec/models/indicator_spec.rb
+++ backend/spec/models/indicator_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Indicator, type: :model do
   subject { build(:indicator) }
 
   it { is_expected.to be_valid }
+
+  it 'should not be valid without name' do
+    subject.name = nil
+    expect(subject).to have(1).errors_on(:name)
+  end
+
+  it 'should not be valid without code' do
+    subject.code = nil
+    expect(subject).to have(1).errors_on(:code)
+  end
+
+  it 'should not be valid without theme' do
+    subject.theme = nil
+    expect(subject).to have(1).errors_on(:theme)
+  end
 end
--- backend/spec/models/indicator_value_spec.rb
+++ backend/spec/models/indicator_value_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe IndicatorValue, type: :model do
   subject { build(:indicator_value) }
 
   it { is_expected.to be_valid }
+
+  it 'should not be valid without value' do
+    subject.value = nil
+    expect(subject).to have(1).errors_on(:value)
+  end
+
+  it 'should not be valid without indicator' do
+    subject.indicator = nil
+    expect(subject).to have(1).errors_on(:indicator)
+  end
 end
