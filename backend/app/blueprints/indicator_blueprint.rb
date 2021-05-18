a02f4a3201dd06c2e9a2c1ac15a3b70691cb12a1
Tomasz Subik <tsubik@gmail.com>
2021-05-18 14:09:20 +0200

    indicator values blueprint

--- backend/app/blueprints/indicator_blueprint.rb
+++ backend/app/blueprints/indicator_blueprint.rb
@@ -3,9 +3,5 @@ class IndicatorBlueprint < Blueprinter::Base
 
   fields :name
 
-  field :values do |i|
-    i.indicator_values.select(:date, :category_1, :category_2, :region, :value).map do |value|
-      value.attributes.transform_keys(&:to_sym).slice(:date, :category_1, :category_2, :region, :value)
-    end
-  end
+  association :indicator_values, name: :values, blueprint: IndicatorValueBlueprint
 end
---
+++ backend/app/blueprints/indicator_value_blueprint.rb
@@ -0,0 +1,3 @@
+class IndicatorValueBlueprint < Blueprinter::Base
+  fields :date, :category_1, :category_2, :region, :value
+end
