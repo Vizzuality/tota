16a30e11ae39d76abccd7d1fa2dda498dd608a63
Tomasz Subik <tsubik@gmail.com>
2021-05-24 20:21:46 +0200

    better examples

--- frontend/constants/themes.ts
+++ frontend/constants/themes.ts
@@ -20,32 +20,19 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus sol
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus sollicitudin, ullamcorper nunc eu, auctor ligula. Sed sodales aliquam nisl eget mollis. Quisque mollis nisi felis, eu convallis purus sagittis sit amet. Sed elementum scelerisque ipsum, at rhoncus eros venenatis at. Donec mattis quis massa ut viverra. In ullamcorper, magna non convallis ultricies, purus magna consectetur sapien, et pulvinar lorem metus vel dolor. Integer in lorem ut dolor tincidunt dictum. Sed metus eros, fermentum ut imperdiet eu, tempor ac nisl. Aenean semper, lacus et dictum laoreet, ipsum eros molestie enim, sed ultrices massa velit id leo. Vivamus dapibus enim eu dolor malesuada, vel consequat felis tempus`,
       },
       {
-        name: 'Monthly domestic (canadian) arrivals to TO (&BC as comparison)',
-        description: 'Lorem ipsum dolora',
+        name: 'Quarterly visits by origin city',
+        description: `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus sollicitudin, ullamcorper nunc eu, auctor ligula. Sed sodales aliquam nisl eget mollis. Quisque mollis nisi felis, eu convallis purus sagittis sit amet. Sed elementum scelerisque ipsum, at rhoncus eros venenatis at. Donec mattis quis massa ut viverra. In ullamcorper, magna non convallis ultricies, purus magna consectetur sapien, et pulvinar lorem metus vel dolor. Integer in lorem ut dolor tincidunt dictum. Sed metus eros, fermentum ut imperdiet eu, tempor ac nisl. Aenean semper, lacus et dictum laoreet, ipsum eros molestie enim, sed ultrices massa velit id leo. Vivamus dapibus enim eu dolor malesuada, vel consequat felis tempus`,
       },
       {
-        name: 'Monthly domestic (canadian) arrivals to TO (&BC as comparison)',
-        description: 'Lorem ipsum dolora',
+        name: 'Monthly Top 10 PRIZM Clusters per month (number of BC visitors to TO)',
+        description: `
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus sollicitudin, ullamcorper nunc eu, auctor ligula. Sed sodales aliquam nisl eget mollis. Quisque mollis nisi felis, eu convallis purus sagittis sit amet. Sed elementum scelerisque ipsum, at rhoncus eros venenatis at. Donec mattis quis massa ut viverra. In ullamcorper, magna non convallis ultricies, purus magna consectetur sapien, et pulvinar lorem metus vel dolor. Integer in lorem ut dolor tincidunt dictum. Sed metus eros, fermentum ut imperdiet eu, tempor ac nisl. Aenean semper, lacus et dictum laoreet, ipsum eros molestie enim, sed ultrices massa velit id leo. Vivamus dapibus enim eu dolor malesuada, vel consequat felis tempus`,
       },
       {
-        name: 'Monthly domestic (canadian) arrivals to TO (&BC as comparison)',
-        description: 'Lorem ipsum dolora',
-      },
-      {
-        name: 'Monthly domestic (canadian) arrivals to TO (&BC as comparison)',
-        description: 'Lorem ipsum dolora',
-      },
-      {
-        name: 'Monthly domestic (canadian) arrivals to TO (&BC as comparison)',
-        description: 'Lorem ipsum dolora',
-      },
-      {
-        name: 'Monthly domestic (canadian) arrivals to TO (&BC as comparison)',
-        description: 'Lorem ipsum dolora',
-      },
-      {
-        name: 'Monthly domestic (canadian) arrivals to TO (&BC as comparison)',
-        description: 'Lorem ipsum dolora',
+        name: '% of annual domestic overnight visitors occuring in peak month & quarter',
+        description: `
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus sollicitudin, ullamcorper nunc eu, auctor ligula. Sed sodales aliquam nisl eget mollis. Quisque mollis nisi felis, eu convallis purus sagittis sit amet. Sed elementum scelerisque ipsum, at rhoncus eros venenatis at. Donec mattis quis massa ut viverra. In ullamcorper, magna non convallis ultricies, purus magna consectetur sapien, et pulvinar lorem metus vel dolor. Integer in lorem ut dolor tincidunt dictum. Sed metus eros, fermentum ut imperdiet eu, tempor ac nisl. Aenean semper, lacus et dictum laoreet, ipsum eros molestie enim, sed ultrices massa velit id leo. Vivamus dapibus enim eu dolor malesuada, vel consequat felis tempus`,
       },
     ],
   },
