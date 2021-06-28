abddff0525d0febaf96478a34ac7758042531d65
Tomasz Subik <tsubik@gmail.com>
2021-06-28 13:54:31 +0200

    test if endpoint is also returning dynamic indicator

--- backend/spec/requests/api/v1/indicators_spec.rb
+++ backend/spec/requests/api/v1/indicators_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'API V1 Indicators', type: :request do
+  before_all do
+    bt1 = create(:business_type, name: 'Accommodation')
+    bt2 = create(:business_type, name: 'Bed & Breakfast', parent: bt1)
+    bt3 = create(:business_type, name: 'Activity / Attraction')
+    r1 = create(:region, name: 'region 1')
+
+    create(:organization, business_type: bt1, region: r1)
+    create(:organization, business_type: bt1, region: r1)
+    create(:organization, business_type: bt2, region: r1)
+    create(:organization, business_type: bt3, region: r1)
+    create(:organization, business_type: bt3, region: r1)
+  end
+
   let_it_be(:visits_by_origin) do
     create(
       :indicator,
@@ -24,6 +37,9 @@ RSpec.describe 'API V1 Indicators', type: :request do
         build(:indicator_value, date: '2020-02', category_1: 'Germany', value: '2222')
       ]
     )
+  }
+  let_it_be(:establishments_by_type) {
+    create(:indicator, slug: 'establishments_by_type')
   }
 
   describe 'GET #index' do
