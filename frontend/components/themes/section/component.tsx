e8c0422f1229d62e3066fb8e376f80227f78afde
Tomasz Subik <tsubik@gmail.com>
2021-06-29 21:40:25 +0200

    use first indicator with mocked data

--- frontend/components/themes/section/component.tsx
+++ frontend/components/themes/section/component.tsx
@@ -1,34 +1,50 @@
 import { FC } from 'react';
+import dynamic from 'next/dynamic';
 
 interface Section {
   name: string;
   description: string;
+  data: any;
+  widget: any;
+}
+
+interface ChartProps {
+  data: any;
+  config: any;
 }
 
 export interface ThemeSectionProps {
   section: Section;
 }
 
-const ThemeSection: FC<ThemeSectionProps> = ({ section }: ThemeSectionProps) => (
-  <div className="mb-10 p-5 bg-white flex">
-    <div className="w-2/5 pr-10 border-r-2">
-      <div className="relative">
-        <div
-          className="absolute rounded-full bg-gray-300 text-gray-700 text-xl h-50 w-50 flex items-center justify-center"
-          style={{ width: 50, height: 50 }}
-        >
-          1
-        </div>
-        <div className="" style={{ marginLeft: 70 }}>
-          <h2 className="text-3xl">{section.name}</h2>
-          <div>sub title</div>
-        </div>
-      </div>
+const ThemeSection: FC<ThemeSectionProps> = ({ section }: ThemeSectionProps) => {
+  const Loading = () => <div>Loading...</div>;
+  const chartType = section.widget?.type || 'pie';
+  const DynamicChart = dynamic<ChartProps>(() => import(`components/charts/${chartType}`), { loading: Loading });
 
-      <p className="mt-10 leading-8">{section.description}</p>
+  const chartData = section.widget?.data(section.data);
+
+  return (
+    <div className="mb-10 p-5 bg-white flex">
+      <div className="w-2/5 pr-10 border-r-2">
+        <div className="relative">
+          <div
+            className="absolute rounded-full bg-gray-300 text-gray-700 text-xl h-50 w-50 flex items-center justify-center"
+            style={{ width: 50, height: 50 }}
+          >
+            1
+          </div>
+          <div className="" style={{ marginLeft: 70 }}>
+            <h2 className="text-3xl">{section.name}</h2>
+            <div>sub title</div>
+          </div>
+        </div>
+
+        <p className="mt-10 leading-8">{section.description}</p>
+      </div>
+      <div className="w-3/5">{chartData && <DynamicChart data={chartData} config={section.widget.config} />}</div>
     </div>
-    <div className="w-3/5">Here will be a chart</div>
-  </div>
-);
+  );
+};
 
 export default ThemeSection;
--- frontend/constants/themes.ts
+++ frontend/constants/themes.ts
@@ -1,3 +1,5 @@
+import { typesOfTourismEstablishments } from './mocks';
+
 export default [
   {
     name: 'Tourism Industry & Arrivals',
@@ -8,7 +10,31 @@ export default [
       {
         name: 'Types of tourism establishments',
         description: `
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus sollicitudin, ullamcorper nunc eu, auctor ligula. Sed sodales aliquam nisl eget mollis. Quisque mollis nisi felis, eu convallis purus sagittis sit amet. Sed elementum scelerisque ipsum, at rhoncus eros venenatis at. Donec mattis quis massa ut viverra. In ullamcorper, magna non convallis ultricies. `,
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus sollicitudin, ullamcorper nunc eu, auctor ligula. Sed sodales aliquam nisl eget mollis. Quisque mollis nisi felis, eu convallis purus sagittis sit amet. Sed elementum scelerisque ipsum, at rhoncus eros venenatis at. Donec mattis quis massa ut viverra. In ullamcorper, magna non convallis ultricies. `,
+        data: typesOfTourismEstablishments.data,
+        widget: {
+          data(rawData: any) {
+            const indicatorData = rawData.filter((x) => x.slug === 'establishments_by_type')[0];
+            if (!indicatorData) return [];
+
+            return indicatorData['indicator_values'].filter((x) => x['category_2'] === 'all');
+          },
+          type: 'pie',
+          config: {
+            margin: {
+              top: 20,
+              right: 0,
+              left: 0,
+              bottom: 0,
+            },
+            pies: [
+              {
+                nameKey: 'category_1',
+              },
+            ],
+            tooltip: {},
+          },
+        },
       },
       {
         name: 'Total of tourism establishments',
