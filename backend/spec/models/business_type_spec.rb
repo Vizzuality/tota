ab59e2dd894b825f91cd6fc5f87dd7b751ca9b6f
Tomasz Subik <tsubik@gmail.com>
2021-07-06 12:42:50 +0200

    add model annotations

--- backend/spec/models/business_type_spec.rb
+++ backend/spec/models/business_type_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: business_types
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  parent_id  :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe BusinessType, type: :model do
