bf3c94679d6c8f61c3f6130581a791ea3144fcc3
Tomasz Subik <tsubik@gmail.com>
2021-08-11 09:57:39 +0200

    set height only for removable tag

--- frontend/components/widgets/charts/common/legend/tag/component.tsx
+++ frontend/components/widgets/charts/common/legend/tag/component.tsx
@@ -8,7 +8,7 @@ import CloseIcon from 'svgs/ui/close.svg?sprite';
 
 const Tag: FC<TagProps> = ({ label, value, color, removable, onRemove }: TagProps) => {
   return (
-    <li key={`item-${value}`} className={cx('flex items-center h-9', { 'border border-gray1 p-1.5': removable })}>
+    <li key={`item-${value}`} className={cx('flex items-center', { 'border border-gray1 p-1.5 h-9': removable })}>
       <div className="w-4 h-4 mr-2 inline-block" style={{ backgroundColor: color }}></div>
       {label}
       {removable && (
