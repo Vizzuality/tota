4e9a4aebe9e11bf2ed83046fd8cd138ac260544c
Tomasz Subik <tsubik@gmail.com>
2021-10-13 17:56:45 +0200

    new dynamic indicators

--- backend/app/services/indicators/employment_by_tourism_region.rb
+++ backend/app/services/indicators/employment_by_tourism_region.rb
@@ -11,12 +11,15 @@ module Indicators
       Indicator.find_by(slug: 'tourism_employment_by_tourism_region_monthly')&.destroy
       Indicator.find_by(slug: 'total_employment_by_tourism_region_annually')&.destroy
       Indicator.find_by(slug: 'total_employment_by_tourism_region_monthly')&.destroy
+      Indicator.find_by(slug: 'tourism_to_total_employment_percentage_annually')&.destroy
+      Indicator.find_by(slug: 'tourism_to_total_employment_percentage_monthly')&.destroy
       generate
     end
 
     def generate
       generate_tourism_employment_data
       generate_total_employment_data
+      generate_tourism_to_total_percentage_data
     end
 
     private
@@ -39,36 +42,79 @@ module Indicators
       generate_annual_average_indicator_values(employment_by_region_annually, employment_by_region_monthly)
     end
 
+    def generate_tourism_to_total_percentage_data
+      monthly = Indicator.create(slug: 'tourism_to_total_employment_percentage_monthly')
+      annually = Indicator.create(slug: 'tourism_to_total_employment_percentage_annually')
+
+      total_monthly = Indicator.find_by(slug: 'total_employment_by_tourism_region_monthly')
+      tourism_monthly = Indicator.find_by(slug: 'tourism_employment_by_tourism_region_monthly')
+
+      total_annually = Indicator.find_by(slug: 'total_employment_by_tourism_region_annually')
+      tourism_annually = Indicator.find_by(slug: 'tourism_employment_by_tourism_region_annually')
+
+      generate_percentage_by_region(monthly, total_monthly, tourism_monthly)
+      generate_percentage_by_region(annually, total_annually, tourism_annually)
+    end
+
     def generate_tourism_region_indicator_values(monthly_indicator, indicator)
       return unless indicator.present?
 
-      indicator
+      values_to_add = indicator
         .indicator_values
         .group_by { |v| [v.region_id, v.date] }
-        .each do |region_date, values|
+        .map do |region_date, values|
           region_id, date = region_date
-          monthly_indicator.indicator_values << IndicatorValue.new(
+          IndicatorValue.new(
+            indicator: monthly_indicator,
             region_id: region_id,
             date: date,
             value: values.sum(0.0, &:value)
           )
         end
+
+      IndicatorValue.import! values_to_add, all_or_none: true
     end
 
     def generate_annual_average_indicator_values(annual_indicator, indicator)
       return unless indicator.present?
 
-      indicator
+      values_to_add = indicator
         .indicator_values
         .group_by { |v| [v.region_id, v.year] }
-        .each do |region_year, values|
+        .map do |region_year, values|
           region_id, year = region_year
-          annual_indicator.indicator_values << IndicatorValue.new(
+          IndicatorValue.new(
+            indicator: annual_indicator,
             region_id: region_id,
             date: year.to_s,
             value: (values.sum(0.0, &:value) / values.length).round
           )
         end
+
+      IndicatorValue.import! values_to_add, all_or_none: true
+    end
+
+    def generate_percentage_by_region(indicator, total, tourism)
+      return unless total.present?
+      return unless tourism.present?
+
+      values_to_add = total
+        .indicator_values
+        .filter_map do |total_value|
+          tourism_value = tourism.indicator_values.find do |iv|
+            iv.region_id == total_value.region_id && iv.date == total_value.date
+          end
+          next unless tourism_value.present?
+
+          IndicatorValue.new(
+            indicator: indicator,
+            region_id: total_value.region_id,
+            date: total_value.date,
+            value: ((tourism_value.value / total_value.value) * 100).round(2)
+          )
+        end
+
+      IndicatorValue.import! values_to_add, all_or_none: true
     end
   end
 end
