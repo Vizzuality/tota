cca597eed44ce7cff9ad272fb4d11174ac3956b2
Tomasz Subik <tsubik@gmail.com>
2021-11-04 16:13:56 +0100

    fix capistrano

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -5,11 +5,13 @@ set :repo_tree, 'backend'
 set :deploy_to, '~/tota-api'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/downloads', 'vendor/bundle', 'db/csvs'
 set :keep_releases, 3
+
 set :rbenv_type, :user
-set :rbenv_ruby, '3.0.1'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, -> { "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:nvm_prefix)} #{fetch(:rbenv_path)}/bin/rbenv exec" }
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
+
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -19,16 +21,13 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
-## Defaults:
-# set :scm,           :git
-# set :branch,        :main
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
 
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :nvm_type, :user
+set :nvm_node, File.read('.nvmrc').strip
+set :nvm_map_bins, %w{node npm yarn}
+set :nvm_roles, :web
+
+set :yarn_flags, '--production --silent --no-progress --frozen-lockfile --no-cache'
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
