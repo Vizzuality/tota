21d6ee83a3e4aeb668043573b541f57f4be9e059
Tomasz Subik <tsubik@gmail.com>
2021-11-04 18:31:04 +0100

    remove not needed code

--- backend/app/javascript/admin/app.js
+++ backend/app/javascript/admin/app.js
@@ -1,13 +1,3 @@
 // Global
 import './modules/bootstrap'
 import './modules/theme'
-import './modules/moment'
-
-// Forms
-/* import "./modules/flatpickr"; */
-
-// Charts
-/* import "./modules/chartjs"; */
-
-// Maps
-/* import "./modules/vector-maps"; */
