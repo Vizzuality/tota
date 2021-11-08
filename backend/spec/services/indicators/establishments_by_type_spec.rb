147c5112d353baf9b9f44b2cfcc31c2ba006691c
Tomasz Subik <tsubik@gmail.com>
2021-11-08 18:12:48 +0100

    more specs

--- backend/spec/services/indicators/establishments_by_type_spec.rb
+++ backend/spec/services/indicators/establishments_by_type_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Indicators::EstablishmentsByType do
       bt2 = create(:business_type, name: 'Bed & Breakfast', parent: bt1)
       bt3 = create(:business_type, name: 'Activity / Attraction')
       bt4 = create(:business_type, name: 'Transportation')
-      @r1 = create(:region, name: 'region 1')
-      @r2 = create(:region, name: 'region 2')
+      @pr = create(:region, :province, name: 'province')
+      @r1 = create(:region, name: 'region 1', parent: @pr)
+      @r2 = create(:region, name: 'region 2', parent: @pr)
 
       create(:organization, business_type: bt1, region: @r1, biosphere_program_member: true)
       create(:organization, business_type: bt1, region: @r1)
@@ -30,16 +31,21 @@ RSpec.describe Indicators::EstablishmentsByType do
         values = indicator.indicator_values.where(category_2: 'all')
         r1_values = values.where(region: @r1)
         r2_values = values.where(region: @r2)
+        pr_values = values.where(region: @pr)
 
-        expect(values.count).to eq(5)
+        expect(values.count).to eq(8)
         expect(r1_values.count).to eq(2)
         expect(r2_values.count).to eq(3)
+        expect(pr_values.count).to eq(3)
         expect(r1_values.where(category_1: 'Accommodation').first.value).to eq(3.0)
         expect(r1_values.where(category_1: 'Activity / Attraction').first.value).to eq(2.0)
         expect(r1_values.where(category_1: 'Transportation').first).to be_nil
         expect(r2_values.where(category_1: 'Accommodation').first.value).to eq(2.0)
         expect(r2_values.where(category_1: 'Activity / Attraction').first.value).to eq(1.0)
         expect(r2_values.where(category_1: 'Transportation').first.value).to eq(1.0)
+        expect(pr_values.where(category_1: 'Accommodation').first.value).to eq(5.0)
+        expect(pr_values.where(category_1: 'Activity / Attraction').first.value).to eq(3.0)
+        expect(pr_values.where(category_1: 'Transportation').first.value).to eq(1.0)
       end
     end
 
@@ -49,16 +55,21 @@ RSpec.describe Indicators::EstablishmentsByType do
         values = indicator.indicator_values.where(category_2: 'biosphere')
         r1_values = values.where(region: @r1)
         r2_values = values.where(region: @r2)
+        pr_values = values.where(region: @pr)
 
-        expect(values.count).to eq(4)
+        expect(values.count).to eq(6)
         expect(r1_values.count).to eq(2)
         expect(r2_values.count).to eq(2)
+        expect(pr_values.count).to eq(2)
         expect(r1_values.where(category_1: 'Accommodation').first.value).to eq(2.0)
         expect(r1_values.where(category_1: 'Activity / Attraction').first.value).to eq(1.0)
         expect(r1_values.where(category_1: 'Transportation').first).to be_nil
         expect(r2_values.where(category_1: 'Accommodation').first.value).to eq(1.0)
         expect(r2_values.where(category_1: 'Activity / Attraction').first.value).to eq(1.0)
         expect(r2_values.where(category_1: 'Transportation').first).to be_nil
+        expect(pr_values.where(category_1: 'Accommodation').first.value).to eq(3.0)
+        expect(pr_values.where(category_1: 'Activity / Attraction').first.value).to eq(2.0)
+        expect(pr_values.where(category_1: 'Transportation').first).to be_nil
       end
     end
 
@@ -67,9 +78,10 @@ RSpec.describe Indicators::EstablishmentsByType do
         indicator = Indicator.find_by(slug: 'total_establishments')
         values = indicator.indicator_values
 
-        expect(values.count).to eq(2)
+        expect(values.count).to eq(3)
         expect(values.find_by(region: @r1).value).to eq(5.0)
         expect(values.find_by(region: @r2).value).to eq(4.0)
+        expect(values.find_by(region: @pr).value).to eq(9.0)
       end
     end
   end
