946d6f7c682b220707277f48959e818dc46f1e3b
Tomasz Subik <tsubik@gmail.com>
2021-11-16 14:06:22 +0100

    remove TurboController is not needed I think

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -14,25 +14,6 @@ Rails.application.reloader.to_prepare do
       %w(html turbo_stream */*).include? request_format.to_s
     end
   end
-
-  class TurboController < ApplicationController
-    class Responder < ActionController::Responder
-      def to_turbo_stream
-        controller.render(options.merge(formats: :html))
-      rescue ActionView::MissingTemplate => error
-        if get?
-          raise error
-        elsif has_errors? && default_action
-          render rendering_options.merge(formats: :html, status: :unprocessable_entity)
-        else
-          redirect_to navigation_location
-        end
-      end
-    end
-
-    self.responder = Responder
-    respond_to :html, :turbo_stream
-  end
 end
 
 
@@ -46,7 +27,7 @@ Devise.setup do |config|
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
-  config.parent_controller = 'TurboController'
+  # config.parent_controller = 'TurboController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -291,7 +272,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
