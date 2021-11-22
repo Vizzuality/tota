7f81924876db6cb7480a3c3eb9c84561a8d363f6
Tomasz Subik <tsubik@gmail.com>
2021-11-22 14:04:05 +0100

    update factories and snapshots

--- backend/spec/factories/development_fund.rb
+++ backend/spec/factories/development_fund.rb
@@ -1,12 +1,24 @@
 FactoryBot.define do
   factory :development_fund do
     project_title { 'Project title' }
+    project_description { 'Description' }
+    recipient { 'Recipient' }
+    lead_organization { 'Lead organization' }
+    location { 'Location' }
+    categories { %w(category1 category2) }
+    scope { 'Scope' }
+    planning_area { 'Planning Area' }
+    second_planning_area { 'Second Planning Area' }
+    total_project_cost { 23_243 }
+    key_funding_amount { 3453 }
+    key_funding_source { 'Source' }
+    funding_subtype { 'Subtype' }
+    funding_call_year { 2020 }
+    funding_call_month { 'January' }
+    project_status { 'In Progress' }
 
     latitude { 50.2323 }
     longitude { 34.2333 }
-
-    key_funding_source { 'Source' }
-    funding_subtype { 'Subtype' }
 
     region
   end
--- backend/spec/factories/organization.rb
+++ backend/spec/factories/organization.rb
@@ -3,6 +3,12 @@ FactoryBot.define do
     name { 'Organization name' }
     latitude { 50.2323 }
     longitude { 34.2333 }
+    external_company_id { 'ID434534' }
+    indigenous_tourism { false }
+    accessibility { false }
+    biosphere_program_member { false }
+    website_url { 'www.example.com' }
+    show_on_platform { true }
 
     region
   end
