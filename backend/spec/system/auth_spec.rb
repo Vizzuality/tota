073c6bcdad872bfbd4297680cc237b2944a33e66
Tomasz Subik <tsubik@gmail.com>
2022-01-13 16:46:06 +0100

    system spec for user reset password flow

--- backend/spec/system/auth_spec.rb
+++ backend/spec/system/auth_spec.rb
@@ -37,4 +37,33 @@ RSpec.describe 'Auth', type: :system do
       expect(page).to have_current_path(new_user_session_path)
     end
   end
+
+  describe 'Password Reset' do
+    it 'works well' do
+      visit '/admin'
+
+      expect(page).to have_text('You need to sign in or sign up before continuing.')
+
+      click_on 'Forgot your password?'
+
+      fill_in :user_email, with: 'admin@example.com'
+      click_on 'Send me reset password instructions'
+
+      email = last_email_to(admin.email)
+      expect(email.subject).to eq('Reset password instructions')
+
+      path_regex = /(?:"https?\:\/\/.*?)(\/.*?)(?:")/
+      reset_link = email.body.match(path_regex)[1]
+
+      visit reset_link
+
+      expect(page).to have_text('Change your password')
+
+      fill_in :user_password, with: 'SuperSecret666'
+      fill_in :user_password_confirmation, with: 'SuperSecret666'
+      click_on 'Change my password'
+
+      expect(page).to have_text('Your password has been changed successfully. You are now signed in.')
+    end
+  end
 end
