44e2949ed8df99dd47a692e4d88e00175a3fee86
Tomasz Subik <tsubik@gmail.com>
2022-01-20 15:22:40 +0100

    add some dummy home page and fix specs, redirect non admins out of admin

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'auth', controllers: { sessions: 'auth/sessions' }
 
-  root to: redirect('admin/dashboards')
+  root to: 'home#index'
 
   namespace :admin do
     root to: 'dashboards#index'
