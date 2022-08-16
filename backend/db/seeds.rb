01bb7b8099e2b26b77bdbb3e3e1d1f35820aa561
Tomasz Subik <tsubik@gmail.com>
2022-08-16 14:43:01 +0200

    add some seeds for additional resources

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -16,4 +16,49 @@ if Rails.env.development?
 
   puts 'Making all widgets public'
   Widget.update_all(public: true)
+
+  puts 'Adding example additional resources'
+
+  AdditionalResource.delete_all
+  AdditionalResourceGroup.delete_all
+
+  doc_group = AdditionalResourceGroup.create!(name: 'Documents from participating regions')
+  link_group = AdditionalResourceGroup.create!(name: 'Interesting links')
+  others_group = AdditionalResourceGroup.create!(name: 'Others')
+
+  Region.province_or_tourism_regions.each do |region|
+    (1..3).to_a.sample.times do |n|
+      r = AdditionalResource.new(title: "#{region.name} - document - #{n}", group: doc_group, region: region, public: true)
+      r.file.attach(io: File.open(Rails.root.join('spec/fixtures/files/test.pdf')), filename: 'test.pdf')
+      r.save!
+    end
+
+    (1..4).to_a.sample.times do |n|
+      AdditionalResource.create!(
+        title: "#{region.name} - link - #{n}",
+        group: link_group,
+        region: region,
+        public: true,
+        link: 'https://example.com'
+      )
+    end
+
+    AdditionalResource.create!(
+      title: "#{region.name} - private link",
+      group: link_group,
+      region: region,
+      public: false,
+      link: 'https://example.com'
+    )
+
+    (1..4).to_a.sample.times do |n|
+      AdditionalResource.create!(
+        title: "#{region.name} - other link - #{n}",
+        group: others_group,
+        region: region,
+        public: true,
+        link: 'https://example.com'
+      )
+    end
+  end
 end
